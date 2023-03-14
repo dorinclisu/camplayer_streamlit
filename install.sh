90ebb4fbda6e6be2d41f2e54fb8bab831b5ee28b
dorin.clisu <dorin.clisu@gmail.com>
2023-03-14 22:17:02 +0200

    Fix install missing requirements

--- install.sh
+++ install.sh
@@ -11,6 +11,7 @@ fi
 # install system dependencies
 apt update
 apt install -y --no-install-recommends build-essential cmake python3-dev python3-pip python3-venv
+apt install -y --no-install-recommends libatlas-base-dev  # for numpy
 
 # install python dependencies
 mkdir -p $LOCATION
@@ -19,7 +20,7 @@ source $LOCATION/venv/bin/activate
 pip install -r requirements.txt
 pip install -r requirements-no-deps.txt --no-dependencies
 
-cp -r src/ $LOCATION/
+cp -r src/. $LOCATION/
 cp camplayer_streamlit /usr/local/bin/
 cp camplayer_streamlit.service /etc/systemd/
 
